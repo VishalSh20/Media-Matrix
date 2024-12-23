efcaee816106cec5941be0cfe256e07c151a76b5
Vishal Sharma <vishal1405sharma@gmail.com>
2024-12-23 19:39:22 +0530

    complete lambda functions for storage and retrieval

---
+++ lambda/app.js
@@ -0,0 +1,52 @@
+// uncomment this in development - also do the same in config.js
+// import { config } from "dotenv";
+// config();
+
+import serverless from "serverless-http";
+import express from "express";
+import cors from "cors";
+import storageRoutes from "./src/routes/storage.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/storage",(_,res,next)=> {
+  storageRoutes(_,res,next)
+});
+
+app.get("/", (_, res, next) => {
+  return res.status(200).json({
+    message: "Hi from the root of the backend!",
+  });
+});
+
+app.get("/healthcheck", (_, res, next) => {
+  return res.status(200).json({
+    message: "OK",
+  });
+});
+
+
+
+app.get("/hello", (_, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// only for local testing
+if(process.env.NODE_ENV !== "production"){
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
+
+export const handler = serverless(app);
---
+++ lambda/src/controllers/storage.controller.js
@@ -0,0 +1,301 @@
+import { GetObjectCommand,PutObjectCommand,DeleteObjectCommand,CopyObjectCommand ,ListObjectsV2Command} from "@aws-sdk/client-s3";
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
+import { s3Client } from "../../config.js";
+
+const validImageTypes = [
+    "image/jpeg",  
+    "image/jpg",   
+    "image/png",   
+    "image/gif",   
+    "image/webp", 
+    "image/bmp",   
+    "image/tiff",
+    "image/svg+xml" 
+  ];
+
+const validVideoTypes = [
+    "video/mp4",
+    "video/avi",
+    "video/mov",
+    "video/wmv",
+    "video/flv",
+    "video/webm"
+];
+
+
+// media related functions - upload, get all, rename, delete
+export async function getMediaUploadURL(req,res){
+    try{
+    const {userId,foldername="",filename,size,type} = req.query;
+
+    if(!userId)
+        return res.status(400).json({error:"User ID is required"});
+
+    if([filename,size,type].some(field => !field))
+        return res.status(400).json({error:"All fields are required"});
+
+    if(!validImageTypes.includes(type) && !validVideoTypes.includes(type))
+        return res.status(400).json({error:"Invalid media type - only images and videos are allowed"});
+
+    const extension = type.substring(type.indexOf("/")+1);
+    const key = (foldername && foldername.length>0) ? `${userId}${foldername}/${filename}-${Date.now()}.${extension}` : `${userId}/${filename}-${Date.now()}.${extension}`;
+    console.log(key);
+    const command = new PutObjectCommand({
+        Bucket:process.env.AWS_BUCKET_NAME,
+        Key:key,
+        ContentType:type,
+        ContentLength:size
+    });
+    const postingURL = await getSignedUrl(s3Client,command);
+    res.status(200).json({url:postingURL,key:key});
+    }
+    catch(err){
+        res.status(500).json({error:err.message});
+    }
+}
+
+export const getAllMedia = async (req,res) => {
+    try {
+        const {userId} = req.query;
+        if(!userId)
+            return res.status(400).json({error:"User ID is required"});  
+
+        const listOptions = {
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Prefix:`${userId}/`,
+        };
+        const command = new ListObjectsV2Command(listOptions);
+        const response = await s3Client.send(command);
+        let files = response.Contents.filter(content => !content.Key.endsWith("/"));
+        console.log(files);
+        for(let file of files){
+            const getCommand = new GetObjectCommand({
+                Bucket:process.env.AWS_BUCKET_NAME,
+                Key:file.Key
+            });
+            const fileURL = await getSignedUrl(s3Client,getCommand);
+            file.url = fileURL;
+            file.filename = file.Key.substring(file.Key.lastIndexOf("/")+1);
+            file.extension = file.Key.substring(file.Key.lastIndexOf(".")+1);
+        }
+        const images = files.filter(file => validImageTypes.includes(`image/${file.extension}`));
+        const videos = files.filter(file => validVideoTypes.includes(`video/${file.extension}`));
+
+        return res.status(200).json({
+            message:"Media fetched successfully",
+            data:{
+                images,
+                videos
+            }});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error:"Internal server error - "+error.message});
+    }
+}
+
+export const renameMedia = async (req,res) => {
+    try {
+        let {userId,oldKey,newKey} = req.query;
+        if(!userId)
+            return res.status(400).json({error:"User ID is required"});  
+        if(!oldKey)
+            return res.status(400).json({error:"Old Key is required"});
+        if(!newKey)
+            return res.status(400).json({error:"New key is required"});
+
+        const copyCommand = new CopyObjectCommand({
+            Bucket:process.env.AWS_BUCKET_NAME,
+            CopySource:`${process.env.AWS_BUCKET_NAME}/${userId}${oldKey}`,
+            Key:`${userId}${newKey}`
+        });
+        await s3Client.send(copyCommand);
+
+        const deleteCommand = new DeleteObjectCommand({
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Key:`${userId}${oldKey}`
+        });
+        await s3Client.send(deleteCommand);
+        return res.status(200).json({message:"Media renamed successfully"});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error:"Internal server error - "+error.message});
+    }
+}
+
+export const deleteMedia = async (req,res) => {
+    try {
+        const {userId,key} = req.query;
+        if(!userId)
+            return res.status(400).json({error:"User ID is required"});  
+        if(!key)
+            return res.status(400).json({error:"Key is required"});
+
+        const deleteCommand = new DeleteObjectCommand({
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Key:`${userId}${key}`
+        });
+        await s3Client.send(deleteCommand);
+        return res.status(200).json({message:"Media deleted successfully"});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error:"Internal server error - "+error.message});
+    }
+}
+
+// getting all contents at a given path
+export const getContentList = async (req,res) => {
+    try {
+        let {userId,key} = req.query;
+        if(!userId)
+            return res.status(400).json({error:"User ID is required"});  
+        if(!key)
+            return res.status(400).json({error:"Key is required"});
+        if(!key.endsWith("/"))
+            key = key + "/";
+
+        const listOptions = {
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Prefix:`${userId}${key}`,
+            Delimiter:"/"
+        };
+        const command = new ListObjectsV2Command(listOptions);
+        const response = await s3Client.send(command);
+        const folders = response.CommonPrefixes.map(prefix => prefix.Prefix.substring((userId+key).length));
+        let files = response.Contents.filter(content => !content.Key.endsWith("/"));
+        for(let file of files){
+            const getCommand = new GetObjectCommand({
+                Bucket:process.env.AWS_BUCKET_NAME,
+                Key:file.Key
+            });
+            const fileURL = await getSignedUrl(s3Client,getCommand);
+            file.url = fileURL;
+            file.filename = file.Key.substring(file.Key.lastIndexOf("/")+1);
+            file.extension = file.Key.substring(file.Key.lastIndexOf(".")+1);
+        }
+
+        return res.status(200).json({
+            message:"Contents fetched successfully",
+            data:{
+                folders,
+                files,
+                KeyCount:response.KeyCount,
+                MaxKeys:response.MaxKeys,
+                IsTruncated:response.IsTruncated,
+                ContinuationToken:response.ContinuationToken,
+                NextContinuationToken:response.NextContinuationToken,
+                StartAfter:response.StartAfter
+            }});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error:"Internal server error - "+error.message});
+    }
+}
+
+// folder related functions - create, rename, delete
+export const createFolder = async (req,res) => {
+    try{
+        let {userId,key} = req.query;
+        if(!userId)
+            return res.status(400).json({error:"User ID is required"});  
+        if(!key)
+            return res.status(400).json({error:"Key is required"});
+       if(!key.endsWith("/"))
+            key = key + "/";
+    
+        const command = new PutObjectCommand({
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Key:`${userId}${key}`,
+            Body:Buffer.from("")
+        });
+        await s3Client.send(command);
+        return res.status(200).json({message:"Folder created successfully"});
+    }
+    catch(error){
+        console.error(error);
+        return res.status(500).json({error:"Internal server error - "+error.message});
+    }
+}
+
+export const renameFolder = async (req,res) => {
+    try{
+    let {userId,oldKey,newKey} = req.query;
+    if(!userId)
+        return res.status(400).json({error:"User ID is required"});  
+    if(!oldKey)
+        return res.status(400).json({error:"Old Key is required"});
+    if(!newKey)
+        return res.status(400).json({error:"New key is required"});
+
+    if(!oldKey.endsWith("/"))
+        oldKey = oldKey + "/";
+    if(!newKey.endsWith("/"))
+        newKey = newKey + "/";
+
+    const listCommand = new ListObjectsV2Command({
+        Bucket:process.env.AWS_BUCKET_NAME,
+        Prefix:`${userId}${oldKey}`
+    });
+    const listResponse = await s3Client.send(listCommand);
+    console.log(listResponse);
+    const objects = listResponse.Contents;
+
+    for(let object of objects){
+        const copyOptions ={
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Key:`${userId}${newKey}${object.Key.substring((userId+oldKey).length)}`,
+            CopySource:`${process.env.AWS_BUCKET_NAME}/${object.Key}`
+        }
+        console.log(copyOptions);
+        const copyCommand = new CopyObjectCommand(copyOptions);
+        await s3Client.send(copyCommand);
+
+        const deleteOptions = {
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Key:`${object.Key}`
+        };
+        console.log(deleteOptions);
+
+        const deleteCommand = new DeleteObjectCommand(deleteOptions);
+        await s3Client.send(deleteCommand);    
+    }
+    
+        return res.status(200).json({message:"Folder renamed successfully"});
+    }
+    catch(error){
+        console.error(error);
+        return res.status(500).json({error:"Internal server error - "+error.message});
+    }
+}
+
+export const deleteFolder = async (req,res) => {
+    try {
+        let {userId,key} = req.query;
+        if(!userId)
+            return res.status(400).json({error:"User ID is required"});  
+        if(!key)
+            return res.status(400).json({error:"Key is required"});
+        if(!key.endsWith("/"))
+            key = key + "/";
+
+        const listCommand = new ListObjectsV2Command({
+            Bucket:process.env.AWS_BUCKET_NAME,
+            Prefix:`${userId}${key}`
+        });
+        const listResponse = await s3Client.send(listCommand);
+        console.log(listResponse);
+        const objects = listResponse.Contents;
+        for(let object of objects){
+            const deleteOptions = {
+                Bucket:process.env.AWS_BUCKET_NAME,
+                Key:`${object.Key}`
+            };
+            const deleteCommand = new DeleteObjectCommand(deleteOptions);
+            await s3Client.send(deleteCommand);
+        }
+        return res.status(200).json({message:"Folder deleted successfully"});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error:"Internal server error - "+error.message});
+    }
+}   
+
